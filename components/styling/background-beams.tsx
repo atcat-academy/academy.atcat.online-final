eedf661a7e7e78ae703c25f1248f026277ba1378
atcat-academy <admin@atcat.online>
2025-12-11 12:24:53 +1000

    beams tweak

--- components/styling/background-beams.tsx
+++ components/styling/background-beams.tsx
@@ -60,7 +60,7 @@ export const BackgroundBeams = React.memo(
     return (
       <div
         className={cn(
-          "absolute  h-full w-full inset-0  [mask-size:40px] [mask-repeat:no-repeat] flex items-center justify-center z-1 pointer-events-none top-[200px] lg:top-[1px]",
+          "absolute  h-full w-full inset-0  [mask-size:40px] [mask-repeat:no-repeat] flex items-center justify-center z-1 pointer-events-none -top-[200px] lg:top-[1px]",
           className
         )}
       >
